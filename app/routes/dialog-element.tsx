b2cc44082a46d2ed9714ca81e0c02996f6f29ada
redasalmi <reda.salmi.elt@gmail.com>
2023-01-25 00:23:56 +0100

    updated dialog animations :p

--- app/routes/dialog-element.tsx
+++ app/routes/dialog-element.tsx
@@ -4,14 +4,27 @@ const animationsTypes = {
   showFromTop: 'open:animate-show-from-top',
   showfromBottom: 'open:animate-show-from-bottom',
   showfromCenter: 'open:animate-show-from-center',
+  hideToTop: 'animate-hide-to-top',
+  hideToBottom: 'animate-hide-to-bottom',
+  hideToCenter: 'animate-hide-to-center',
 } as const;
 
-type animation =
-  | typeof showFromTop
-  | typeof showfromBottom
-  | typeof showfromCenter;
+const {
+  showFromTop,
+  showfromBottom,
+  showfromCenter,
+  hideToTop,
+  hideToBottom,
+  hideToCenter,
+} = animationsTypes;
 
-const { showFromTop, showfromBottom, showfromCenter } = animationsTypes;
+type AnimationKeys = keyof typeof animationsTypes;
+type Animation = typeof animationsTypes[AnimationKeys];
+
+const isAnimation = (animation: any): animation is Animation => {
+  return Object.values(animationsTypes).includes(animation);
+};
+
 const animationsOptions = [
   { value: showFromTop, label: 'Show Dialog from Top' },
   { value: showfromBottom, label: 'Show Dialog from Bottom' },
@@ -21,42 +34,66 @@ const animationsOptions = [
 export default function DialogElementRoute() {
   const dialogRef = React.useRef<HTMLDialogElement>(null!);
   const dialogContentRef = React.useRef<HTMLDivElement>(null!);
-  const [dialogAnimation, setDialogAnimation] =
-    React.useState<animation>(showFromTop);
 
   React.useEffect(() => {
+    let closeTimer: NodeJS.Timeout;
+
     const clickOutside = (event: MouseEvent) => {
       const { target } = event;
-      if (target && target instanceof Node) {
-        if (!dialogContentRef.current.contains(target)) {
-          closeDialog();
+
+      if (
+        dialogRef.current.open &&
+        target instanceof Node &&
+        !dialogContentRef.current.contains(target)
+      ) {
+        if (dialogRef.current.classList.contains(showFromTop)) {
+          updateDialogAnimation(hideToTop);
+        } else if (dialogRef.current.classList.contains(showfromBottom)) {
+          updateDialogAnimation(hideToBottom);
+        } else if (dialogRef.current.classList.contains(showfromCenter)) {
+          updateDialogAnimation(hideToCenter);
         }
 
-        return;
+        closeTimer = setTimeout(() => {
+          dialogRef.current.close();
+          document.body.style.overflow = 'initial';
+        }, 600);
       }
-
-      closeDialog();
     };
 
     document.body.addEventListener('click', clickOutside);
 
     return () => {
       document.body.removeEventListener('click', clickOutside);
+      clearTimeout(closeTimer);
     };
   }, []);
 
-  const openDialog = () => dialogRef.current.showModal();
-  const closeDialog = () => dialogRef.current.close();
+  const openDialog = () => {
+    document.body.style.overflow = 'hidden';
+
+    if (dialogRef.current.classList.contains(hideToTop)) {
+      updateDialogAnimation(showFromTop);
+    } else if (dialogRef.current.classList.contains(hideToBottom)) {
+      updateDialogAnimation(showfromBottom);
+    } else if (dialogRef.current.classList.contains(hideToCenter)) {
+      updateDialogAnimation(showfromCenter);
+    }
+
+    dialogRef.current.showModal();
+  };
+
+  const updateDialogAnimation = (animation: Animation) => {
+    dialogRef.current.classList.remove(...Object.values(animationsTypes));
+    dialogRef.current.classList.add(animation);
+  };
 
   const handleAnimationChange = (
     event: React.ChangeEvent<HTMLSelectElement>,
   ) => {
-    if (
-      event.target.value === showFromTop ||
-      event.target.value === showfromBottom ||
-      event.target.value === showfromCenter
-    ) {
-      setDialogAnimation(event.target.value);
+    const animation = event.target.value;
+    if (isAnimation(animation)) {
+      updateDialogAnimation(animation);
     }
   };
 
@@ -68,7 +105,6 @@ export default function DialogElementRoute() {
       >
         Open Dialog
       </button>
-
       <select
         className="ml-4 h-10 rounded-md px-4 py-2"
         onChange={handleAnimationChange}
@@ -82,7 +118,7 @@ export default function DialogElementRoute() {
 
       <dialog
         ref={dialogRef}
-        className={`absolute inset-0 m-auto h-96 w-96 rounded-md p-0 shadow backdrop:bg-gray-400 backdrop:bg-opacity-70 ${dialogAnimation}`}
+        className={`absolute inset-0 m-auto h-96 w-96 rounded-md p-0 shadow backdrop:bg-gray-400 backdrop:bg-opacity-70 ${animationsOptions[0].value}`}
       >
         <div ref={dialogContentRef} className="h-full p-2" />
       </dialog>
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,11 +16,26 @@ module.exports = {
           from: { width: '6rem', height: '6rem' },
           to: { width: '24rem', height: '24rem' },
         },
+        'hide-to-top': {
+          from: { transform: 'translateY(0%)' },
+          to: { transform: 'translateY(-150%)' },
+        },
+        'hide-to-bottom': {
+          from: { transform: 'translateY(0%)' },
+          to: { transform: 'translateY(110%)' },
+        },
+        'hide-to-center': {
+          from: { width: '24rem', height: '24rem' },
+          to: { width: '6rem', height: '6rem' },
+        },
       },
       animation: {
         'show-from-top': 'show-from-top 0.6s ease-in-out',
         'show-from-bottom': 'show-from-bottom 0.6s ease-in-out',
         'show-from-center': 'show-from-center 0.6s ease-in-out',
+        'hide-to-top': 'hide-to-top 0.6s ease-in-out',
+        'hide-to-bottom': 'hide-to-bottom 0.6s ease-in-out',
+        'hide-to-center': 'hide-to-center 0.6s ease-in-out',
       },
     },
   },
