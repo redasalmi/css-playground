41c03c85f4f074fa8737b40754338883a708e7d6
redasalmi <reda.salmi.elt@gmail.com>
2023-01-26 20:17:36 +0100

    added dialog from button animation

--- app/routes/dialog-element.tsx
+++ app/routes/dialog-element.tsx
@@ -2,36 +2,46 @@ import * as React from 'react';
 
 const animationsTypes = {
   showFromTop: 'open:animate-show-from-top',
-  showfromBottom: 'open:animate-show-from-bottom',
-  showfromCenter: 'open:animate-show-from-center',
+  showFromBottom: 'open:animate-show-from-bottom',
+  showFromCenter: 'open:animate-show-from-center',
+  showFromButton: 'open:animate-show-from-button',
   hideToTop: 'animate-hide-to-top',
   hideToBottom: 'animate-hide-to-bottom',
   hideToCenter: 'animate-hide-to-center',
+  hideToButton: 'animate-hide-to-button',
 } as const;
 
 const {
   showFromTop,
-  showfromBottom,
-  showfromCenter,
+  showFromBottom,
+  showFromCenter,
+  showFromButton,
   hideToTop,
   hideToBottom,
   hideToCenter,
+  hideToButton,
 } = animationsTypes;
 
 type AnimationKeys = keyof typeof animationsTypes;
 type Animation = (typeof animationsTypes)[AnimationKeys];
 
-const isAnimation = (animation: any): animation is Animation => {
+function isAnimation(animation: any): animation is Animation {
   return Object.values(animationsTypes).includes(animation);
-};
+}
+
+function valueInPx(value: number) {
+  return `${parseFloat(value.toString())}px`;
+}
 
 const animationsOptions = [
   { value: showFromTop, label: 'Show Dialog from Top' },
-  { value: showfromBottom, label: 'Show Dialog from Bottom' },
-  { value: showfromCenter, label: 'Show Dialog from Center' },
+  { value: showFromBottom, label: 'Show Dialog from Bottom' },
+  { value: showFromCenter, label: 'Show Dialog from Center' },
+  { value: showFromButton, label: 'Show Dialog from Button' },
 ];
 
 export default function DialogElementRoute() {
+  const openBtnRef = React.useRef<HTMLButtonElement>(null!);
   const dialogRef = React.useRef<HTMLDialogElement>(null!);
   const dialogContentRef = React.useRef<HTMLDivElement>(null!);
 
@@ -39,10 +49,12 @@ export default function DialogElementRoute() {
     let closeTimer: NodeJS.Timeout;
     const dialog = dialogRef.current;
 
-    const clickOutside = (event: MouseEvent | Event) => {
-      event.preventDefault();
-      const { target } = event;
+    const clickOutside = (event: Event, preventDefault?: Boolean) => {
+      if (preventDefault) {
+        event.preventDefault();
+      }
 
+      const { target } = event;
       if (
         dialog.open &&
         target instanceof Node &&
@@ -50,46 +62,60 @@ export default function DialogElementRoute() {
       ) {
         if (dialog.classList.contains(showFromTop)) {
           updateDialogAnimation(hideToTop);
-        } else if (dialog.classList.contains(showfromBottom)) {
+        } else if (dialog.classList.contains(showFromBottom)) {
           updateDialogAnimation(hideToBottom);
-        } else if (dialog.classList.contains(showfromCenter)) {
+        } else if (dialog.classList.contains(showFromCenter)) {
           updateDialogAnimation(hideToCenter);
+        } else if (dialog.classList.contains(showFromButton)) {
+          updateDialogAnimation(hideToButton);
         }
 
         closeTimer = setTimeout(() => {
           dialog.close();
           document.body.style.overflow = 'initial';
-        }, 600);
+        }, 290);
       }
     };
 
-    dialog.addEventListener('cancel', clickOutside);
     document.body.addEventListener('click', clickOutside);
+    dialog.addEventListener('cancel', (event) => clickOutside(event, true));
 
     return () => {
-      dialog.removeEventListener('cancel', clickOutside);
       document.body.removeEventListener('click', clickOutside);
+      dialog.removeEventListener('cancel', clickOutside);
       clearTimeout(closeTimer);
     };
   }, []);
 
   const openDialog = () => {
+    const dialog = dialogRef.current;
     document.body.style.overflow = 'hidden';
 
-    if (dialogRef.current.classList.contains(hideToTop)) {
+    if (dialog.classList.contains(hideToTop)) {
       updateDialogAnimation(showFromTop);
-    } else if (dialogRef.current.classList.contains(hideToBottom)) {
-      updateDialogAnimation(showfromBottom);
-    } else if (dialogRef.current.classList.contains(hideToCenter)) {
-      updateDialogAnimation(showfromCenter);
+    } else if (dialog.classList.contains(hideToBottom)) {
+      updateDialogAnimation(showFromBottom);
+    } else if (dialog.classList.contains(hideToCenter)) {
+      updateDialogAnimation(showFromCenter);
+    } else if (dialog.classList.contains(hideToButton)) {
+      updateDialogAnimation(showFromButton);
     }
 
-    dialogRef.current.showModal();
+    dialog.showModal();
   };
 
   const updateDialogAnimation = (animation: Animation) => {
-    dialogRef.current.classList.remove(...Object.values(animationsTypes));
-    dialogRef.current.classList.add(animation);
+    const dialog = dialogRef.current;
+    const openBtn = openBtnRef.current;
+
+    const { top, left, width, height } = openBtn.getBoundingClientRect();
+    dialog.style.setProperty('--btn-top', valueInPx(top));
+    dialog.style.setProperty('--btn-left', valueInPx(left));
+    dialog.style.setProperty('--btn-width', valueInPx(width));
+    dialog.style.setProperty('--btn-height', valueInPx(height));
+
+    dialog.classList.remove(...Object.values(animationsTypes));
+    dialog.classList.add(animation);
   };
 
   const handleAnimationChange = (
@@ -104,6 +130,7 @@ export default function DialogElementRoute() {
   return (
     <div className="mt-8">
       <button
+        ref={openBtnRef}
         onClick={() => openDialog()}
         className="h-10 rounded-md bg-blue-500 px-4 py-2 text-white"
       >
@@ -122,7 +149,7 @@ export default function DialogElementRoute() {
 
       <dialog
         ref={dialogRef}
-        className={`absolute inset-0 m-auto h-96 w-96 rounded-md p-0 shadow backdrop:bg-gray-400 backdrop:bg-opacity-70 ${animationsOptions[0].value}`}
+        className={`absolute inset-0 m-auto h-96 w-96 rounded-md p-0 shadow backdrop:bg-gray-400 backdrop:bg-opacity-70 ${showFromTop}`}
       >
         <div ref={dialogContentRef} className="h-full p-2" />
       </dialog>
